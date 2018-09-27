7b7557cf060e88758ff8faed385dbe600b4df016
Wolf Behrenhoff <wolf@behrenhoff.de>
2018-09-28 01:31:55 +0200

    Store message,line,col in DB & improve indent

--- parse-clang-tidy-log.pl
+++ parse-clang-tidy-log.pl
@@ -4,24 +4,12 @@ use 5.18.0;
 use warnings;
 use Data::Dumper;
 
-my $fileremove = '/home/behrenhoff/root-head/';
-my $disabledCheckersRE = 'cppcoreguidelines-owning-memory|modernize|hicpp-use-auto|hicpp-no-array-decay|hicpp-vararg|readability-non-const-parameter|google-readability-namespace-comments|hicpp-use-nullptr|google-readability-casting|cppcoreguidelines-pro-type-cstyle-cast|hicpp-signed-bitwise';
-
-
-# my @dirs = qw(core doc documentation geom graf2d graf3d gui hist interpreter io macros main man math misc montecarlo net proof roofit rootx sql test tmva tree tutorials);
-
-# my $filelimit = shift @ARGV;
-
-sub useFile {
-    my $fn = shift;
-    #/home/behrenhoff/root-head/src/
-        return 1;
-    #return $fn =~ m!/tmva/tmva/!;
-#     return $fn =~ m!$filelimit!;
-}
+my $fileremove = '/home/behrenhoff/root-head/src/';
+my $disabledCheckersRE = 'cppcoreguidelines-owning-memory|hicpp-use-auto|hicpp-no-array-decay|hicpp-vararg|readability-non-const-parameter|google-readability-namespace-comments|hicpp-use-nullptr|google-readability-casting|cppcoreguidelines-pro-type-cstyle-cast';
+#'modernize|hicpp-signed-bitwise';
 
 sub checkerIsDiabled {
-   return shift =~ /$disabledCheckersRE/;
+    return shift =~ /$disabledCheckersRE/;
 }
 
 sub parseInput {
@@ -34,21 +22,23 @@ sub parseInput {
         my ($file, $position, $message, $checker);
         open my $FH, '<', $filename or die $!;
         while (my $line = <$FH>) {
-           if ($line =~ m#(/home\S+):(\d+:\d+): (.+)\[(.+?)(?:,-warnings-as-errors)\]$#) {
-                 if ($file) {
-                    if (useFile($file) && !checkerIsDiabled($checker)) {
-                       ++$checkerCount{$checker} unless exists $files{$file}{$position}{$checker};
-                       $files{$file}{$position}{$checker} = "$message\n$verbose"
+            if ($line =~ m#(/home\S+):(\d+:\d+): (.+)\[(.+?)(?:,-warnings-as-errors)?\]$#) {
+                if ($file) {
+                    if (!checkerIsDiabled($checker)) {
+                        ++$checkerCount{$checker} unless exists $files{$file}{$position}{$checker};
+                        $files{$file}{$position}{$checker} = "$message\n$verbose"
                     }
                     $verbose = '';
-                 }
-                 ($file, $position, $message, $checker) = ($1, $2, $3, $4);
-                 $file =~ s/\Q$fileremove//;
-           } else {
-                 if ($line !~ m# -quiet /home#) {
+                }
+                ($file, $position, $message, $checker) = ($1, $2, $3, $4);
+                #print Dumper [($1, $2, $3, $4)];
+                $file =~ s/\Q$fileremove//;
+            } else {
+                # print Dumper ["No match: ", $line];
+                if ($line !~ m# -quiet /home#) {
                     $line =~ s/\Q$fileremove//;
                     $verbose .= $line if $file;
-                 }
+                }
             }
         }
     }
@@ -79,8 +69,8 @@ for my $checker (@sortedCheckers) {
 
 use DBI;
 my $dbh = DBI->connect("dbi:SQLite:dbname=result.sqlite","","", {AutoCommit=>0});
-$dbh->do("CREATE TABLE IF NOT EXISTS result (file TEXT, topdir TEXT, checker TEXT, position TEXT)");
-my $dbInsert = $dbh->prepare("INSERT INTO result VALUES (?,?,?,?)");
+$dbh->do("CREATE TABLE IF NOT EXISTS result (file TEXT, topdir TEXT, checker TEXT, line INTEGER, col INTEGER, code TEXT)");
+my $dbInsert = $dbh->prepare("INSERT INTO result VALUES (?,?,?,?,?,?)");
 
 
 open my $testjs, '>', "test.js" or die $!;
@@ -90,11 +80,12 @@ RESULT: for my $showChecker (@sortedCheckers) {
     for my $filename (sort keys %$files) {
         my $positionHref = $files->{$filename};
         for my $position (map $_->[0], sort {$a->[1] <=> $b->[1]} map {[$_, (split/:/)[0]]} keys %$positionHref) {
+            my ($posLine, $posCol) = split /:/, $position;
             my $checkerHref = $positionHref->{$position};
             for my $checker (sort keys %$checkerHref) {
                 next if $checker ne $showChecker;
-                if ($filename =~ m#^src/(\w+)/.*\.cxx$#) {
-                    $dbInsert->execute($filename, $1, $checker, $position);
+                if ($filename =~ m#^(\w+)/#) {
+                    $dbInsert->execute($filename, $1, $checker, $posLine, $posCol, $checkerHref->{$checker});
                 }
                 print $testjs "[";
                 print $testjs join ", ", 
